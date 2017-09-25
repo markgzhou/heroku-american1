18dac1a3dbb069893e7df2b0aadda7ca486f0bfa
markzhou <markzhou@outlook.com>
2017-09-25 14:24:54 -0500

    show ip at index

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
- hello from American-1 Server
+<?
+
+if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+  $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+}
+
+echo $_SERVER['REMOTE_ADDR'];
+
+?>
