e7c0936565d51f629e93356a6fc40da50e08af92
markzhou <markzhou@carfax.com>
2018-01-22 12:44:35 -0600

    Fixed IPs from Heroku internal servers

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
   $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
 }
+else if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])){
+	$ipAddresses = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+    $_SERVER['REMOTE_ADDR'] = trim(end($ipAddresses));
+}
 
 echo $_SERVER['REMOTE_ADDR'];
 
